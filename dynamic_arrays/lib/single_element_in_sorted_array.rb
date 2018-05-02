67d7f0b198c0516f5253d807862b85c50abf67bc
Ujwal Battar <ujwal.battar@gmail.com>
2018-05-01 19:50:22 -0700

    work on arrays

---
+++ dynamic_arrays/lib/single_element_in_sorted_array.rb
@@ -0,0 +1,12 @@
+# [1, 1, 2, 3, 3, 4, 4, 5, 5] => 2
+# [1, 1, 2, 3] [3, 4, 4, 5, 5]
+
+# def single_element(arr)
+#   mid = arr.length / 2
+#   left = arr.take(mid)
+#   right = arr.drop(mid)
+#
+#
+# end
+
+ 
---
+++ dynamic_arrays/lib/well_formed_string.rb
@@ -0,0 +1,10 @@
+# write a function to test if a string is
+
+([]),{[]}
+
+def perfect_string(string)
+  
+
+  stack = []
+  string.chars.each {|c|}
+end
