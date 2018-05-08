9e979f8af718f34831c00cce22bbdec092ec8272
Ujwal Battar <ujwal.battar@gmail.com>
2018-05-08 14:51:01 -0700

    graphs

--- graphs_topological_sort/lib/install_order.rb
+++ graphs_topological_sort/lib/install_order.rb
@@ -7,24 +7,47 @@
 
 # Import any files you need to
 
-# Got help, need to learn during review
+# require_relative 'graph'
+# require_relative 'topological_sort'
+#
+# def install_order(arr)
+#   vertices = []
+#   max_id = 0
+#   arr.each do |el|
+#     max_id = el[0] if el[0] > max_id
+#     max_id = el[1] if el[1] > max_id
+#   end
+#   (1..max_id).each do |value|
+#     vertices << Vertex.new(value)
+#   end
+#   edges = []
+#   arr.each do |el|
+#     edges << Edge.new(vertices[el[0] - 1], vertices[el[1] - 1])
+#   end
+#   topological_sort(vertices).map(&:value).reverse!
+# end
+
 
 require_relative 'graph'
 require_relative 'topological_sort'
 
 def install_order(arr)
-  vertices = []
-  max_id = 0
-  arr.each do |el|
-    max_id = el[0] if el[0] > max_id
-    max_id = el[1] if el[1] > max_id
+  vertices = {}
+  max = 0
+  
+  arr.each do |tuple|
+    vertices[tuple[0]] = Vertex.new(tuple[0]) unless vertices[tuple[0]]
+    vertices[tuple[1]] = Vertex.new(tuple[1]) unless vertices[tuple[1]]
+    Edge.new(vertices[tuple[1]], vertices[tuple[0]])
+
+    max = tuple.max if tuple.max > max
   end
-  (1..max_id).each do |value|
-    vertices << Vertex.new(value)
+
+  independent = []
+
+  (1..max).each do |i|
+    independent << i unless vertices[i]
   end
-  edges = []
-  arr.each do |el|
-    edges << Edge.new(vertices[el[0] - 1], vertices[el[1] - 1])
-  end
-  topological_sort(vertices).map(&:value).reverse!
+
+  independent + topological_sort(vertices.values).map {|v| v.value}
 end
---
+++ graphs_topological_sort/lib/install_order2.rb
@@ -0,0 +1,14 @@
+def install_order2(arr)
+  max = 0
+  vartices = {}
+  arr.each do |tuple|
+    dependent = tuple[0]
+    dependency = tuple[1]
+
+    vertices[dependent] = Vertex.new(dependent) unless vertices[dependent]
+    vertices[dependent] = Vertex.new(dependency) if dependency && !vertices[dependency]
+    Edge.new(vertices[dependency], vertices[dependent]) fi dependency
+  end
+
+  topological_sort(vertices.values).map {|v| v.value }
+end
