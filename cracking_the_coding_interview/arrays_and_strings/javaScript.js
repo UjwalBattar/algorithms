8cf29ecd42e028915e662f45e26696e041bbb999
Ujwal Battar <ujwal.battar@gmail.com>
2018-05-24 19:34:08 -0700

    work

--- cracking_the_coding_interview/arrays_and_strings/javaScript.js
+++ cracking_the_coding_interview/arrays_and_strings/javaScript.js
@@ -16,3 +16,16 @@ function uniqueCharString(string) {
   }
   return true;
 }
+
+function uniqueCharString2(string) {
+  string.sort();
+
+  for (let i = 0; i < string.length - 1; i++) {
+    if (string[i] === string[1 + 1]) return false;
+    return true;
+  }
+}
+
+// 1.2 Write code to reverse a C-Style String
+// (C-String means that “abcd” is represented as five characters,
+// including the null character )
--- cracking_the_coding_interview/arrays_and_strings/ruby.rb
+++ cracking_the_coding_interview/arrays_and_strings/ruby.rb
@@ -18,3 +18,7 @@ def unique_char_string(string)
   end
   true
 end
+
+# 1.2 Write code to reverse a C-Style String
+# (C-String means that “abcd” is represented as five characters,
+# including the null character )
