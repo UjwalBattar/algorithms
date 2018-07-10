fda3dc956665528e2ae2c660623cec0a469ca369
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-09 17:02:51 -0700

    isPermutationPalindrome JavaScript

--- cracking_the_coding_interview/arrays_and_strings/javaScript.js
+++ cracking_the_coding_interview/arrays_and_strings/javaScript.js
@@ -75,3 +75,34 @@ function isPalindromePermutation(string) {
   if (count > 1) return false;
   return true;
 }
+
+// 1.4 Palindrome Permutation: Given a string, write a function to check
+// if it is a permutation of a palindrome. A palindrome is a word or
+// phrase that is the same forwards and backwards. A permutation is a
+// rearrangement of letters. The palindrome does not need to be limited to
+// just dictionary words.
+//
+// EXAMPLE
+// Input: Tact Coa
+// Output: True (permutations: "taco cat". "atco cta". etc.)
+// Hints: #106, #121, #134, #136
+
+function isPermutationPalindrome(s) {
+  let charHash = {};
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") continue;
+    let c = s[i].toLowerCase().charCodeAt(0);
+    if (charHash[c] % 2 === 1) {
+      charHash[c]++;
+      count--;
+    } else if (charHash[c] % 2 === 0) {
+      charHash[c]++;
+      count++;
+    } else {
+      charHash[c] = 1;
+      count++;
+    }
+  }
+  return count <= 1;
+}
