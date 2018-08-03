d5a7c54d88522193f9d369fde80fa5dec5bf40fc
Ujwal Battar <ujwal.battar@gmail.com>
2018-08-02 22:23:26 -0700

    graph setup

---
+++ graphs_topological_sort/sol/install_order.rb
@@ -0,0 +1,53 @@
+# Given an Array of tuples, where tuple[0] represents a package id,
+# and tuple[1] represents its dependency, determine the order in which
+# the packages should be installed. Only packages that have dependencies
+# will be listed, but all packages from 1..max_id exist.
+
+# N.B. this is how `npm` works.
+
+# Import any files you need to
+
+# require_relative 'graph'
+# require_relative 'topological_sort'
+#
+# def install_order(arr)
+#   vertices = []
+#   max_id = 0
+#   arr.each do |el|
+#     max_id = el[0] if el[0] > max_id
+#     max_id = el[1] if el[1] > max_id
+#   end
+#   (1..max_id).each do |value|
+#     vertices << Vertex.new(value)
+#   end
+#   edges = []
+#   arr.each do |el|
+#     edges << Edge.new(vertices[el[0] - 1], vertices[el[1] - 1])
+#   end
+#   topological_sort(vertices).map(&:value).reverse!
+# end
+
+
+require_relative 'graph'
+require_relative 'topological_sort'
+
+def install_order(arr)
+  vertices = {}
+  max = 0
+  
+  arr.each do |tuple|
+    vertices[tuple[0]] = Vertex.new(tuple[0]) unless vertices[tuple[0]]
+    vertices[tuple[1]] = Vertex.new(tuple[1]) unless vertices[tuple[1]]
+    Edge.new(vertices[tuple[1]], vertices[tuple[0]])
+
+    max = tuple.max if tuple.max > max
+  end
+
+  independent = []
+
+  (1..max).each do |i|
+    independent << i unless vertices[i]
+  end
+
+  independent + topological_sort(vertices.values).map {|v| v.value}
+end
---
+++ graphs_topological_sort/sol/topological_sort.rb
@@ -0,0 +1,123 @@
+# require_relative 'graph'
+#
+# # Implementing topological sort using both Khan's and Tarian's algorithms
+
+# def topological_sort(vertices)
+#   sorted = []
+#   queue = []
+#   degrees = {}
+#
+#   vertices.each do |vertex|
+#     if vertex.in_edges.empty?
+#       queue.unshift(vertex)
+#     end
+#     degrees[vertex] = vertex.in_edges.count
+#   end
+#   until queue.empty?
+#     current = queue.pop
+#     current.out_edges.each do |edge|
+#       destination = edge.to_vertex
+#
+#       degrees[destination] -= 1
+#       queue.unshift(destination) if degrees[destination] == 0
+#     end
+#
+#     vertices.delete(current)
+#     sorted << current
+#   end
+#
+#   sorted.length == 2 ? [] : sorted
+# end
+
+# Khans algorithm
+#
+# require_relative 'graph'
+
+def topological_sort(vertices)
+  order = []
+  queue = []
+  in_edges = {}
+
+  vertices.each do |vertex|
+    in_edge_cost = vertex.in_edges.reduce(0) {|sum, edge| sum += edge.cost}
+    in_edges[vertex] = in_edge_cost
+    queue << vertex if in_edge_cost == 0
+  end
+
+  until queue.empty?
+    current - queue.shift
+
+    current.out_edges .each do |edge|
+      to_vertex = edge.to_vertex
+      in_edges[to_vertex] -= edge.cost
+      queue << to_vertex if in_edges[to_vertex] == 0
+    end
+
+    order << current
+  end
+
+  vertices.length == order.length ? order : []
+end
+
+
+# Tarjan's Algorithm
+# require_relative 'graph'
+# require 'set'
+#
+# def topological_sort(vertices)
+#   order = []
+#   explored = Set.new
+#
+#   vertices.each do |vertex|
+#     dfs!(order, explored, vertex) unless explored.inclkude?(vertex)
+#   end
+#
+#   order
+# end
+#
+#
+# def dfs!(order, explored, vertex)
+#   explored.add(vertex)
+#
+#   vertex.out_edges.each do |edge|
+#     to_vertex = edge.to_vertex
+#     dfs!(order, explored, to_vertex) unless explored.include?(to_vertex)
+#   end
+#
+#   order.unshift(vertex)
+# end
+
+# Tarjan's algorithm (with cycle catching)
+
+require_relative 'graph'
+require 'set'
+
+def topological_sort(vertices)
+  order = []
+  explored = Set.new
+  cycle = false
+  temp = Set.new
+
+  vertices.each do |vertex|
+    cycle = dfs!(order, explored, vertex, cycle, temp) unless explored.include?(vertex)
+    return [] if cycle
+  end
+
+  order
+end
+
+
+def dfs!(order, explored, vertex, cycle, temp)
+  return true if temp.include?(vertex)
+  temp.add(vertex)
+
+  vertex.out_edges.each do |edge|
+    to_vertex = edge.to_vertex
+    cycle = dfs!(order, explored, to_vertex, cycle, temp) unless explored.include?(to_vertex)
+    return true if cycle
+  end
+
+  temp.delete(vertex)
+  explored.add(vertex)
+  order.unshift(vertex)
+end
