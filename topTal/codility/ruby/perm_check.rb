cf3de360c9a61b46b96382eb11c81abafb01a945
Ujwal Battar <ujwal.battar@gmail.com>
2018-08-29 20:07:24 -0700

    perm check

---
+++ topTal/codility/ruby/perm_check.rb
@@ -0,0 +1,64 @@
+# A non-empty array A consisting of N integers is given.
+#
+# A permutation is a sequence containing each element from 1 to N
+# once, and only once.
+#
+# For example, array A such that:
+#
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# is a permutation, but array A such that:
+#
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# is not a permutation, because value 2 is missing.
+#
+# The goal is to check whether array A is a permutation.
+#
+# Write a function:
+#
+# def solution(A);
+#
+# that, given an array A, returns 1 if array A is a permutation and
+# 0 if it is not.
+#
+# For example, given array A such that:
+#
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# the function should return 1.
+#
+# Given array A such that:
+#
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# the function should return 0.
+#
+# Write an efficient algorithm for the following assumptions:
+#
+# N is an integer within the range [1..100,000];
+# each element of array A is an integer within the range
+# [1..1,000,000,000].
+
+def perm_check(arr)
+  n = arr.length
+  sum = (n * (n + 1)) / 2
+  nums = {}
+
+  n.times do |i|
+    return 0 if nums[arr[i]]
+    sum -= arr[i]
+    nums[arr[i]] = true
+  end
+
+  sum == 0 ? 1 : 0
+end
+
+perm_check([4, 1, 3, 2])
+perm_check([4, 1, 3])
