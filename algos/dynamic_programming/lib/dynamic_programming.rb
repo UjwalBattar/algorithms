4c731d70de9febed129815e6a90dab62884b2cb5
Ujwal Battar <ujwal.battar@gmail.com>
2018-11-02 20:10:11 -0700

    blairs nums and frog hops

--- algos/dynamic_programming/lib/dynamic_programming.rb
+++ algos/dynamic_programming/lib/dynamic_programming.rb
@@ -1,27 +1,70 @@
 class DynamicProgramming
 
   def initialize
-
+    @blair_cache = {1 => 1, 2 => 2}
+    @frog_cache = {1 => [[1]], 2 => [[1, 1], [2]], 3 => [[1, 1, 1], [1, 2], [2, 1], [3]]}
   end
 
   def blair_nums(n)
+    # recursive
+    # return 1 if n == 1
+    # return 2 if n == 2
+    # return blair_nums(n - 1) + blair_nums(n - 2) + (2 * (n - 1) - 1)
 
+    # dynamic programming: top down
+    return @blair_cache[n] if @blair_cache[n]
+    res = blair_nums(n - 1) + blair_nums(n - 2) + (2 * (n - 1) - 1)
+    @blair_cache[n] = res
+    res
+
+    # dynamic programming: bottom up
+    # cache = blair_cache_builder(n)
+    # cache[n]
   end
 
-  def frog_hops_bottom_up(n)
+  # bottom up blair helper
+  # def blair_cache_builder(n)
+  #   cache = {1 => 1, 2 => 2}
+  #   return cache if n < 3
+  #   (3..n).each do |i|
+  #     cache[i] = cache[i - 1] + cache[i - 2] + (2 * (i - 1) - 1)
+  #   end
+  #   cache
+  # end
 
+  def frog_hops_bottom_up(n)
+    cache = frog_cache_builder(n)
+    cache[n]
   end
 
   def frog_cache_builder(n)
-
+    cache = {1 => [[1]], 2 => [[1, 1], [2]], 3 => [[1, 1, 1], [1, 2], [2, 1], [3]]}
+    return cache if cache[n]
+    4.upto(n) do |i|
+      cache[i] = []
+      1.upto(i - 1) do |j|
+        cache[j].each do |x|
+          1.upto(3) do |y|
+            cache[i].concat([x + [y]]) if [x + [y]][0].reduce(:+) == i
+          end
+        end
+      end
+    end
+    cache
   end
 
   def frog_hops_top_down(n)
-
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
-
+    return @frog_cache if @frog_cache[n]
+    result = []
+    1.upto(3) do |i|
+      result.concat(frog_hops_top_down_helper(n - i).map {|j| j + [i]})
+    end
+    @frog_cache[n] = result
+    result
   end
 
   def super_frog_hops(n, k)
@@ -29,14 +72,47 @@ class DynamicProgramming
   end
 
   def knapsack(weights, values, capacity)
-
+    @knapsack = { 0 => Array.new(weights.length, 0) }
+    knapsack_table(weights, values, capacity)
+    # p @knapsack.sort_by {|k,v| k} #checks cache
+    @knapsack[capacity].last
   end
 
   # Helper method for bottom-up implementation
+  # you must select the optimum from between these two paradigms: 
+    # use either the previous item solution at this capacity, 
+    # or the previous item solution from a smaller bag plus this 
+    # item's value.
   def knapsack_table(weights, values, capacity)
+    return 0 if capacity <= 0 || weights.length == 0
 
+    i = weights.length -   1
+
+    @knapsack[capacity] = [] if @knapsack[capacity].nil?
+
+    return @knapsack[capacity][i] if @knapsack[capacity][i]
+
+    prev = knapsack_table(weights[0...i], values[0...i], capacity)
+    other = knapsack_table(weights[0...i], values[0...i], capacity - weights[i]) + values[i]
+    return prev if weights[i] > capacity
+    choices = [prev, other]
+    max = choices.max
+    @knapsack[capacity][i] = max
+    max
   end
 
   def maze_solver(maze, start_pos, end_pos)
   end
 end
+
+# knapsack([23, 31, 29, 44, 53, 38, 63, 85, 89, 82 ], [ 92, 57, 49, 68, 60, 43, 67, 84, 87, 72 ], 23)
+
+
+# def test(n)
+#   a = Time.now
+#   res = blair_nums(n)
+#   b = Time.now
+#   c = b - a
+#   print "TIME ~~ #{c}"
+#   return res
+# end
--- algos/dynamic_programming/sol/dynamic_programming.rb
+++ algos/dynamic_programming/sol/dynamic_programming.rb
@@ -5,6 +5,7 @@ class DynamicProgramming
     @blair_cache = {1 => 1, 2 => 2}
     @frog_cache = {1 => [[1]], 2 => [[2],[1,1]], 3 => [[1,2],[2,1],[1,1,1],[3]]}
     @super_cache = {0 => [[]], 1 => [[1]]}
+    @maze_cache = {}
   end
 
   def blair_nums(n)
@@ -67,13 +68,83 @@ class DynamicProgramming
 
 
   def knapsack(weights, values, capacity)
-    
+    @knapsack = { 0 => Array.new(weights.length, 0) }
+    knapsack_table(weights, values, capacity)
+    # p @knapsack.sort_by {|k,v| k} #checks cache
+    @knapsack[capacity].last
   end
 
+  # Helper method for bottom-up implementation
+  # you must select the optimum from between these two paradigms: use either the previous item solution at this capacity, or the previous item solution from a smaller bag plus this item's value.
   def knapsack_table(weights, values, capacity)
+    return 0 if capacity <= 0 || weights.length == 0
 
+    i = weights.length -   1
+
+    @knapsack[capacity] = [] if @knapsack[capacity].nil?
+
+    return @knapsack[capacity][i] if @knapsack[capacity][i]
+
+    prev = knapsack_table(weights[0...i], values[0...i], capacity)
+    other = knapsack_table(weights[0...i], values[0...i], capacity - weights[i]) + values[i]
+    return prev if weights[i] > capacity
+    choices = [prev, other]
+    max = choices.max
+    @knapsack[capacity][i] = max
+    max
   end
 
   def maze_solver(maze, start_pos, end_pos)
+    build_cache(start_pos)
+    solve_maze(maze, start_pos, end_pos)
+    find_path(end_pos)
   end
+
+  private
+
+  def solve_maze(maze, start_pos, end_pos)
+    return true if start_pos == end_pos
+
+    get_moves(maze, start_pos).each do |next_pos|
+      unless @maze_cache.keys.include?(next_pos)
+        @maze_cache[next_pos] = start_pos
+        solve_maze(maze, next_pos, end_pos)
+      end
+    end
+  end
+
+  def build_cache(start_pos)
+    @maze_cache[start_pos] = nil
+  end
+
+  def find_path(end_pos)
+    path = []
+    current = end_pos
+
+    until current.nil?
+      path.unshift(current)
+      current = @maze_cache[current]
+    end
+
+    path
+  end
+
+  def get_moves(maze, from_pos)
+    directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+    x, y = from_pos
+    result = []
+
+    directions.each do |dx, dy|
+      next_pos = [x + dx, y + dy]
+      result << next_pos if is_valid_pos?(maze, next_pos)
+    end
+
+    result
+  end
+
+  def is_valid_pos?(maze, pos)
+    x, y = pos
+    x >= 0 && y >= 0 && x < maze.length && y < maze.first.length && maze[x][y] != "X"
+  end
+
 end
